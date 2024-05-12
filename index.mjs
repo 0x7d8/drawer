9df8f02e0201166ae033d14bf20cf5ebb5a2da3b
0x7d8 <rjansengd@gmail.com>
2024-05-12 11:45:35 +0200

    some speed improvements

--- index.mjs
+++ index.mjs
@@ -1,19 +1,6 @@
-import webserver from "rjweb-server"
+import { Server } from "rjweb-server"
 
-const routes = new webserver.routeList()
-routes.routeBlock('/')
-  .static('static', {
-    addTypes: true,
-    hideHTML: true
-  })
-
-routes.event('notfound', async(ctr) => {
-  return ctr.status(404).print('404 Not Found')
-}); routes.event('request', async(ctr) => {
-  console.log(`${ctr.url.method} Request made to ${ctr.url.pathname}`)
-})
-
-const controller = webserver.initialize({
+const server = new Server({
   port: 8000,
   body: {
     enabled: false
@@ -21,7 +8,18 @@ const controller = webserver.initialize({
   proxy: true
 })
 
-controller.setRoutes(routes)
-controller.start().then((res) => {
+server.prefix('/')
+  .static('static', {
+    addTypes: true,
+    hideHTML: true
+  })
+
+server.event('notfound', async(ctr) => {
+  return ctr.status(404).print('404 Not Found')
+}).event('request', async(ctr) => {
+  console.log(`${ctr.url.method} Request made to ${ctr.url.pathname}`)
+})
+
+server.start().then((res) => {
   console.log(`webserver started on port ${res.port}`)
 })
